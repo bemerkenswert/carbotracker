89e5c14e360cbeaf8e2d8e0c51dfd4295ad63275
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2023-09-18 19:39:22 +0200

    Delete all Tests

--- libs/ui/src/lib/fixed-position.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FixedPositionDirective } from './fixed-position.directive';
-
-describe('FixedPositionDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FixedPositionDirective();
-    expect(directive).toBeTruthy();
-  });
-});
