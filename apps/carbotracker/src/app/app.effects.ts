75ed5f85c94ddcdbaa3ac3edc84f0db67df2ef5c
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2023-11-22 20:36:22 +0100

    fix import

--- apps/carbotracker/src/app/app.effects.ts
+++ apps/carbotracker/src/app/app.effects.ts
@@ -3,7 +3,9 @@ import { Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, from, map, of, switchMap } from 'rxjs';
 import { AuthApiActions } from '../auth-feature/auth.actions';
-import { SignUpFormComponentActions } from '../login-feature/login.actions';
+import {
+  SignUpApiActions
+} from '../login-feature/login.actions';
 import { AppRouterEffectsActions } from './app.actions';
 
 export const navigateToProducts$ = createEffect(
@@ -18,7 +20,7 @@ export const navigateToProducts$ = createEffect(
 export const navigateToApp$ = createEffect(
   (actions$ = inject(Actions), router = inject(Router)) =>
     actions$.pipe(
-      ofType(SignUpFormComponentActions.signUpSuccessful),
+      ofType(SignUpApiActions.signUpSuccessful),
       switchMap(() =>
         from(router.navigate(['app'])).pipe(
           map(() => AppRouterEffectsActions.navigationToAppSuccessful()),
