c5d89dc94d7c1721ae1cf89fd072a62237965f23
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2024-01-12 19:38:47 +0100

    fix account page

--- apps/carbotracker/src/settings-feature/pages/AccountPage/account-page.component.ts
+++ apps/carbotracker/src/settings-feature/pages/AccountPage/account-page.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
 import { Store } from '@ngrx/store';
 import { take } from 'rxjs';
 import { authFeature } from '../../../auth-feature/auth.reducer';
@@ -13,29 +11,28 @@ import { AccountPageActions } from '../../settings.actions';
 const createAccountFormGroup = () =>
   inject(FormBuilder).nonNullable.group({
     email: '',
-    password: '',
   });
 
 @Component({
-  selector: 'carbotracker-settings-page',
+  selector: 'carbotracker-account-page',
   standalone: true,
   imports: [
     MatButtonModule,
-    MatIconModule,
-    MatListModule,
-    MatFormFieldModule,
     MatInputModule,
+    MatFormFieldModule,
     ReactiveFormsModule,
   ],
   templateUrl: './account-page.component.html',
   styleUrls: ['./account-page.component.scss'],
 })
 export class AccountPageComponent implements OnInit {
-  private store = inject(Store);
   protected readonly accountFormGroup = createAccountFormGroup();
-  private email$ = this.store.select(authFeature.selectEmail).pipe(take(1));
+  private readonly store = inject(Store);
+  private readonly email$ = this.store
+    .select(authFeature.selectEmail)
+    .pipe(take(1));
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.email$.subscribe((email) => {
       if (email) {
         return this.accountFormGroup.controls.email.setValue(email);
@@ -43,10 +40,12 @@ export class AccountPageComponent implements OnInit {
     });
   }
 
-  onSaveChanges() {
-    const { email, password } = this.accountFormGroup.getRawValue();
-    this.store.dispatch(
-      AccountPageActions.saveChangesClicked({ email, password }),
-    );
+  protected onSaveChanges() {
+    const { email } = this.accountFormGroup.getRawValue();
+    this.store.dispatch(AccountPageActions.saveChangesClicked({ email }));
+  }
+
+  protected onFocusPasswordInput() {
+    this.store.dispatch(AccountPageActions.passwordInputFocused());
   }
 }
