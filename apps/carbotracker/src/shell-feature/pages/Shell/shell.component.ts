eddf662adc083653504093dd51bc86b03c8c135e
Marianna Maglio <mariannabellamia@gmail.com>
2024-02-07 09:28:53 +0100

    style headers and change from menu to icons in navigation bar

--- apps/carbotracker/src/shell-feature/pages/Shell/shell.component.ts
+++ apps/carbotracker/src/shell-feature/pages/Shell/shell.component.ts
@@ -1,20 +1,18 @@
-import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { Component, Signal, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { getRouterSelectors } from '@ngrx/router-store';
 import { Store, createSelector } from '@ngrx/store';
-import { isHandset } from '../../../util/breakpoint';
 import { ShellComponentActions } from '../../shell.actions';
 
 interface NavItem {
   onClick: () => void;
   isActive: Signal<boolean>;
-  name: string;
+  icon: string;
 }
 
 const selectIsProductsRoute = createSelector(
@@ -27,18 +25,28 @@ const selectIsCurrentMealRoute = createSelector(
   (url): boolean => url.startsWith('/app/current-meal'),
 );
 
+const selectIsSettingsRoute = createSelector(
+  getRouterSelectors().selectUrl,
+  (url): boolean => url.startsWith('/app/settings'),
+);
+
 const getNavItems = () => {
   const store = inject(Store);
   return [
     {
-      name: 'products',
       onClick: () => store.dispatch(ShellComponentActions.productsClicked()),
       isActive: store.selectSignal(selectIsProductsRoute),
+      icon: 'lunch_dining',
     },
     {
-      name: 'current meal',
       onClick: () => store.dispatch(ShellComponentActions.currentMealClicked()),
       isActive: store.selectSignal(selectIsCurrentMealRoute),
+      icon: 'restaurant',
+    },
+    {
+      onClick: () => store.dispatch(ShellComponentActions.settingsClicked()),
+      isActive: store.selectSignal(selectIsSettingsRoute),
+      icon: 'settings',
     },
   ];
 };
@@ -47,26 +55,17 @@ const getNavItems = () => {
   selector: 'carbotracker-shell',
   standalone: true,
   imports: [
-    AsyncPipe,
+    CommonModule,
     MatButtonModule,
     MatIconModule,
-    MatListModule,
     MatSidenavModule,
     MatToolbarModule,
     NgFor,
-    NgIf,
-    RouterLinkActive,
     RouterOutlet,
   ],
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss'],
 })
 export class ShellComponent {
-  protected readonly isHandset = isHandset();
   protected readonly navItems: NavItem[] = getNavItems();
-  private readonly store = inject(Store);
-
-  protected onSettings(): void {
-    this.store.dispatch(ShellComponentActions.settingsClicked());
-  }
 }
