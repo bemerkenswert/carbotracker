a5d7bc8c90d94dd60466375ea06a6392ff614f63
Steffen Merk <33038091+SteffenLm@users.noreply.github.com>
2024-05-09 20:10:21 +0200

    remove error log (#25)

--- apps/carbotracker/src/main.ts
+++ apps/carbotracker/src/main.ts
@@ -1,13 +1,6 @@
-import { isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-
-if (isDevMode()) {
-  console.error(
-    'Product page not working after direct login, F5 fixes the issue temporarily!',
-  );
-}
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
