f60bc476eaf2a7eda11d362029ab5c96b69b46f4
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2024-05-09 13:02:29 +0200

    Format Files

--- libs/ui/src/lib/toolbar.component.ts
+++ libs/ui/src/lib/toolbar.component.ts
@@ -7,18 +7,18 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [MatIconModule],
   template: `<div>
       @if (showBackwardNavigation) {
-      <mat-icon
-        (click)="backwardNavigation.emit()"
-        style="margin-right: 0.5rem; cursor: pointer"
-        >chevron_left</mat-icon
-      >
+        <mat-icon
+          (click)="backwardNavigation.emit()"
+          style="margin-right: 0.5rem; cursor: pointer"
+          >chevron_left</mat-icon
+        >
       }
       <h3>{{ title }}</h3>
       <span style="flex: 1 1 auto"></span>
       <ng-content></ng-content>
     </div>
     @if (subTitle) {
-    <h6>{{ subTitle }}</h6>
+      <h6>{{ subTitle }}</h6>
     }`,
   styles: [
     'div { display: flex; flex-direction: row; align-items: center; } h6 { margin: 0; }',
