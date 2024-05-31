d42a520ce604f137e974db87712bfd6876a8bdfc
Marianna Maglio <marianna.maglio@web.de>
2024-05-31 11:18:33 +0200

    inject mat dialog data to component

--- libs/ui/src/lib/confirmation-dialog/confirmation-dialog.component.ts
+++ libs/ui/src/lib/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+
+export interface ConfirmationDialogConfig {
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'ctui-confirmation-dialog',
   standalone: true,
+  imports: [MatDialogModule],
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.scss'],
 })
 export class ConfirmationDialogComponent {
-    onAbortClick() {
-        console.log('test')
-    }
-    
-    onConfirmClick() {
-        console.log('test')
-    }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ConfirmationDialogConfig) {}
+
+  onAbortClick() {
+    console.log('test');
+  }
+
+  onConfirmClick() {
+    console.log('test');
+  }
 }
