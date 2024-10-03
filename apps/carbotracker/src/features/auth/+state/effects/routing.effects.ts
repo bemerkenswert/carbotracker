364cd980b1400656734f0e8985800eda62eb3a80
Steffen Merk <33038091+SteffenLm@users.noreply.github.com>
2024-10-03 16:00:27 +0200

    fix auth bug (#82)

--- apps/carbotracker/src/features/auth/+state/effects/routing.effects.ts
+++ apps/carbotracker/src/features/auth/+state/effects/routing.effects.ts
@@ -1,8 +1,10 @@
+import { DOCUMENT } from '@angular/common';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { createEffect, Actions, ofType } from '@ngrx/effects';
-import { switchMap, from, map, catchError, of } from 'rxjs';
-import { LogoutApiActions } from '../actions/api.actions';
+import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
+import { routerNavigatedAction } from '@ngrx/router-store';
+import { catchError, from, map, of, switchMap, tap } from 'rxjs';
+import { LoginApiActions, LogoutApiActions } from '../actions/api.actions';
 import {
   LoginFormComponentActions,
   SignUpFormComponentActions,
@@ -40,4 +42,26 @@ export const navigateToLoginPage$ = createEffect(
       ),
     ),
   { functional: true },
+);
+
+/**
+ * Reload App is needed because of a strange firebase bug.
+ * After login, the initial products don't load. Also on logout
+ * the app crashes. Remove this effect to debug further. At the
+ * moment the app get's reloaded in the following cases:
+ * 1. login success -> navigation to other view --> reload
+ * 2. logout success -> navigation to other view --> reload
+ *
+ */
+export const reloadApp$ = createEffect(
+  (actions$ = inject(Actions), document = inject(DOCUMENT)) =>
+    actions$.pipe(
+      ofType(
+        LoginApiActions.loginSuccessful,
+        LogoutApiActions.logoutSuccessful,
+      ),
+      concatLatestFrom(() => actions$.pipe(ofType(routerNavigatedAction))),
+      tap(() => document.location.reload()),
+    ),
+  { functional: true, dispatch: false },
 );
